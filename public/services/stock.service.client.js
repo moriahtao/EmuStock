d2a82cdfffb612b8a6e177da8360f438d6ab66b1
Fan Yang <wyns.smart@gmail.com>
2016-12-01 16:35:18 -0500

    added: StockService

---
+++ public/services/stock.service.client.js
@@ -0,0 +1,44 @@
+(function () {
+    angular
+        .module('EmuStock')
+        .factory('StockService', StockService)
+
+    function StockService($http) {
+        return {
+            lookup: lookup,
+            quote: quote,
+            chart: chart,
+        }
+
+        function urlBuilder(fn, key, value) {
+            var base = 'http://dev.markitondemand.com/MODApis/Api/v2/';
+            var jsonp = '/jsonp?callback=JSON_CALLBACK&';
+            return base + fn + jsonp + key + '=' + value;
+        }
+
+        function lookup(input) {
+            return $http.jsonp(urlBuilder('Lookup', 'input', input));
+        }
+
+        function quote(symbol) {
+            return $http.jsonp(urlBuilder('Quote', 'symbol', symbol));
+        }
+
+        function chart(symbol) {
+            var chartReq = JSON.stringify({
+                Normalized: false,
+                NumberOfDays: 30,
+                DataPeriod: 'Day',
+                Elements: [
+                    {
+                        Symbol: symbol,
+                        Type: 'price',
+                        Params: ["ohlc"],
+                    }
+                ],
+            });
+            return $http.jsonp(urlBuilder('InteractiveChart', 'parameters', chartReq));
+        }
+    }
+
+})();
