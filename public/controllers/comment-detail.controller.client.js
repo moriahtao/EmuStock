bdf8c3b11cf3ee32ab107c38938ada7a931fc40d
ChienliMa <maqianlie@gmail.com>
2016-12-05 16:02:59 -0500

    add comment controller

---
+++ public/controllers/comment-detail.controller.client.js
@@ -0,0 +1,71 @@
+(function () {
+    angular
+        .module('EmuStock')
+        .controller('CommentDetailController', CommentDetailController);
+
+    function CommentDetailController($routeParams, CommentService) {
+        var vm = this;
+
+        vm.self_uid = $routeParams.self_uid;
+        vm.other_uid = $routeParams.other_uid;
+
+        vm.self = null;
+        vm.other = null;
+
+        // get user profile to know whether this stock is followed
+        UserService.findUserById(vm.self_uid)
+            .then(
+                function(res){
+                    vm.self = res.data;
+                    if(vm.other) {
+                        setFollowed(); // in the end of file
+                    }
+                }
+            );
+
+        UserService.findUserById(vm.other_uid)
+            .then(
+                function(res){
+                    vm.other = res.data;
+                    if(vm.self) {
+                        setFollowed(); // in the end of file
+                    }
+                }
+            );
+
+        // functions
+        vm.follow = function() {
+            UserService.followUser(vm.self_uid, vm.other_uid)
+                .then(
+                    function() {
+                        alert("follow success.");
+                        vm.other.followed = true;
+                    },
+                    function() {
+                        vm.other.followed = false;
+                        alert("follow failed. try again later");
+                    }
+                );
+        };
+
+        vm.unfollow = function() {
+            UserService.unfollowUser(vm.self_uid, vm.other_uid)
+                .then(
+                    function() {
+                        alert("unfollow success.");
+                        vm.other.followed = false;
+                    }
+                );
+        };
+
+        // helper function
+        function setFollowed(){
+            for(var i=0; i<vm.self.followings; i++){
+                if(vm.other_uid == vm.self.followings[i]) {
+                    vm.other.followed = true;
+                    break
+                }
+            }
+        }
+    }
+})();
---
+++ public/controllers/comment-list.controller.client.js
@@ -0,0 +1,34 @@
+// this controller can be reused in 4 situations:
+// 1. self.following
+// 2. self.followed
+// 3. other.following
+// 4. other.followed
+
+// We do not allow follow and unfollow operation
+// user can only follow/unfollow user in detail page
+
+(function () {
+    angular
+        .module('EmuStock')
+        .controller('CommentListController', CommentListController);
+
+    function CommentListController($location, $routeParams, CommentService) {
+        var vm = this;
+        var path = $location.path();
+        vm.uid = $routeParams.uid;
+        vm.users = [];
+
+        // initialization : get user profile to know whether this stock is followed
+        UserService.findUserById(vm.uid)
+            .then(
+                function(res){
+                    if (path.include("followings")) {
+                        vm.users = res.data.followings;
+                    }
+                    if (path.include("followers")) {
+                        vm.users = res.data.followers;
+                    }
+                }
+            );
+    }
+})();
