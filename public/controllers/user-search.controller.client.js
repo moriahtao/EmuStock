be677b7b88ad10844e487314f695191a619823fd
ChienliMa <maqianlie@gmail.com>
2016-12-11 14:54:14 -0500

    small change to user-search controller

--- public/controllers/user-search.controller.client.js
+++ public/controllers/user-search.controller.client.js
@@ -6,22 +6,24 @@
         .module('EmuStock')
         .controller('UserSearchController', UserSearchController);
 
-    function UserSearchController($routeParams, SharedService, UserService) {
+    function UserSearchController(SharedService, UserService) {
         const vm = this;
         vm.shared = SharedService;
         vm.shared.initController(vm, init);
 
         function init() {
             vm.term = ""; // used for user search
-            vm.search = function() {
-                UserService.searchUserByUsername(vm.term)
-                    .then(
-                        function(res) {
-                            vm.users = res.data;
-                        }
-                    )
-            };
+            vm.search = search;
         }
+
+        function search() {
+            UserService.searchUserByUsername(vm.term)
+                .then(
+                    function(res) {
+                        vm.users = res.data;
+                    }
+                )
+        };
 
     }
 })();
