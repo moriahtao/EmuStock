016d6fc8c36a1bc178b50eaf041138362e189cb1
Fan Yang <wyns.smart@gmail.com>
2016-12-10 19:31:25 -0500

    restyled

--- public/controllers/register.controller.client.js
+++ public/controllers/register.controller.client.js
@@ -13,26 +13,26 @@
         vm.user = {};
 
         vm.register = function () {
-            if(vm.myForm.$invalid == true){
+            if (vm.myForm.$invalid == true) {
                 vm.error = "Please check and resubmit!";
                 vm.alert = "* Required Field";
                 vm.vpsdAlert = "Passwords don't match!";
-            } else if(vm.user.password !== vm.user.verpass){
+            } else if (vm.user.password !== vm.user.verpass) {
                 vm.error = "Please check and resubmit!";
                 vm.alert = "* Required Field";
                 vm.vpsdAlert = "Passwords don't match!";
-            }else{
-            return UserService.register(vm.user)
-                .then(
-                    function () {
-                        console.log("Register success");
-                        $location.url(vm.shared.getRoute('profile'));
-                    },
-                    function (err) {
-                        console.warn(err.data);
-                    }
-                );
-        };
+            } else {
+                return UserService.register(vm.user)
+                    .then(
+                        function () {
+                            console.log("Register success");
+                            $location.url(vm.shared.getRoute('profile'));
+                        },
+                        function (err) {
+                            console.warn(err.data);
+                        }
+                    );
+            }
         }
     }
 })();
